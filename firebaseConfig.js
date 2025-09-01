768b5fb9a26d3c2973c4be2bf28688ddb49bb21e
choijooonil123 <choijooonil123@gmail.com>
2025-09-01 10:02:21 +0900

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjAvWTltlBDiU52womJHTQM8YQl4sBUws",
+  authDomain: "bible-read-53201.firebaseapp.com",
+  projectId: "bible-read-53201",
+  storageBucket: "bible-read-53201.firebasestorage.app",
+  messagingSenderId: "927625955045",
+  appId: "1:927625955045:web:319ac478c2d4952733b60c",
+  measurementId: "G-7K0XVSDWVR"
+};
